02d0fb45253a6daa464c3d2f25d05c79b1e1a20b
Evan Summers <evan.summers@gmail.com>
2014-05-25 03:15:02 +0200

    list iterator with maximum count

--- src/vellum/util/Lists.java
+++ src/vellum/util/Lists.java
@@ -345,4 +345,10 @@ public class Lists {
         return list.toArray();
     }
 
+    public static <T> List<T> reverse(List<T> list) {
+        List<T> reversedList = new ArrayList(list);
+        Collections.reverse(reversedList);
+        return reversedList;
+    }
+
 }

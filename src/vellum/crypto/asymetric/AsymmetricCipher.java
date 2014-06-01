10079751803e0a2896ebeb6491c2d0c185f5dda4
Evan Summers <evan.summers@gmail.com>
2014-06-01 15:18:54 +0200

    Hmac utility class

--- src/vellum/crypto/asymetric/AsymmetricCipher.java
+++ src/vellum/crypto/asymetric/AsymmetricCipher.java
@@ -1,3 +1,23 @@
+/*
+ * Source https://github.com/evanx by @evanxsummers
+
+       Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements. See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership. The ASF licenses this file to
+       you under the Apache License, Version 2.0 (the "License").
+       You may not use this file except in compliance with the
+       License. You may obtain a copy of the License at:
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.  
+ */
 package vellum.crypto.asymetric;
 
 import java.security.GeneralSecurityException;
@@ -99,11 +119,11 @@ public final class AsymmetricCipher {
         return decryptCipher;
     }
 
-    public byte[] encrypt(byte[] bytes) throws Exception {
+    public byte[] encrypt(byte[] bytes) throws GeneralSecurityException {
         return createEncryptCipher().doFinal(bytes);
     }
     
-    public byte[] decrypt(byte[] bytes) throws Exception {
+    public byte[] decrypt(byte[] bytes) throws GeneralSecurityException {
         return createDecryptCipher().doFinal(bytes);
     }
 }
---
+++ src/vellum/crypto/hmac/Hmac.java
@@ -0,0 +1,60 @@
+/*
+ * Source https://github.com/evanx by @evanxsummers
+
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements. See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership. The ASF licenses this file to
+ you under the Apache License, Version 2.0 (the "License").
+ You may not use this file except in compliance with the
+ License. You may obtain a copy of the License at:
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.  
+ */
+package vellum.crypto.hmac;
+
+import java.security.GeneralSecurityException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import vellum.util.Base64;
+
+public final class Hmac {
+    final String algorithm = "HmacSHA256";
+    
+    SecretKey secretKey;
+
+    public Hmac() {
+    }
+
+    public Hmac(String encoded) {
+        secretKey = new SecretKeySpec(Base64.decode(encoded), algorithm);
+    }
+
+    public void generateSecret() throws GeneralSecurityException {
+        KeyGenerator keyGen = KeyGenerator.getInstance(algorithm);
+        secretKey = keyGen.generateKey();
+    }
+
+    public SecretKey getSecretKey() {
+        return secretKey;
+    }
+
+    public String encodedSecretKey() {
+        return Base64.encode(secretKey.getEncoded());
+    }
+
+    public byte[] mac(byte[] message) throws GeneralSecurityException {
+        Mac mac = Mac.getInstance(secretKey.getAlgorithm());
+        mac.init(secretKey);
+        return mac.doFinal(message);
+    }
+}

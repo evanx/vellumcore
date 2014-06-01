b9e72ec8fabe918e3a6216446c24ed449c94d084
Evan Summers <evan.summers@gmail.com>
2014-06-01 15:27:49 +0200

    hmac test

---
+++ test/vellum/crypto/HmacTest.java
@@ -0,0 +1,23 @@
+package vellum.crypto;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import vellum.crypto.hmac.Hmac;
+
+public class HmacTest {
+    static Logger logger = LoggerFactory.getLogger(HmacTest.class); 
+    
+    Hmac hmac = new Hmac();
+
+    @Test
+    public void test() throws Exception {
+        hmac.generateSecret();
+        String text = "Let's test this baby...";
+        String mac = hmac.mac(text);
+        logger.info("mac {}", mac);
+        hmac = new Hmac(hmac.encodeSecret());
+        Assert.assertEquals(mac, hmac.mac(text));
+    }
+}
--- test/vellum/crypto/asymmetric/AsymmetricCipherTest.java
+++ test/vellum/crypto/asymmetric/AsymmetricCipherTest.java
@@ -2,10 +2,14 @@ package vellum.crypto.asymmetric;
 
 import junit.framework.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import vellum.crypto.asymetric.AsymmetricCipher;
 import vellum.util.Base64;
 
 public class AsymmetricCipherTest {
+    static Logger logger = LoggerFactory.getLogger(AsymmetricCipherTest.class); 
+    
     AsymmetricCipher cipher = new AsymmetricCipher(2048);
 
     @Test
@@ -13,7 +17,7 @@ public class AsymmetricCipherTest {
         cipher.generateKeyPair();
         String text = "Let's test this baby...";
         byte[] encryptedBytes = cipher.encrypt(text.getBytes());
-        System.out.println("encrypted: " + Base64.encode(encryptedBytes));
+        logger.info("encrypted {}", Base64.encode(encryptedBytes));
         byte[] decryptedBytes = cipher.decrypt(encryptedBytes);
         String decryptedText = new String(decryptedBytes);
         Assert.assertEquals(text, decryptedText);

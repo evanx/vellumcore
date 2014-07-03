c834fe2c75ffb648cd2d457ba1e2d304807e28f8
evans <evans@saratoga.co.za>
2014-07-03 18:34:46 +0200

    Exceptions message formatting if null

--- src/vellum/exception/Exceptions.java
+++ src/vellum/exception/Exceptions.java
@@ -1,22 +1,22 @@
 /*
  * Source https://github.com/evanx by @evanxsummers
 
-       Licensed to the Apache Software Foundation (ASF) under one
-       or more contributor license agreements. See the NOTICE file
-       distributed with this work for additional information
-       regarding copyright ownership. The ASF licenses this file to
-       you under the Apache License, Version 2.0 (the "License").
-       You may not use this file except in compliance with the
-       License. You may obtain a copy of the License at:
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements. See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership. The ASF licenses this file to
+ you under the Apache License, Version 2.0 (the "License").
+ You may not use this file except in compliance with the
+ License. You may obtain a copy of the License at:
 
-         http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-       Unless required by applicable law or agreed to in writing,
-       software distributed under the License is distributed on an
-       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-       KIND, either express or implied.  See the License for the
-       specific language governing permissions and limitations
-       under the License.  
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.  
  */
 package vellum.exception;
 
@@ -33,44 +33,48 @@ import vellum.util.Args;
  */
 public class Exceptions {
 
-    public static Throwable getThrowable(Object[] args) {
-        if (args.length > 0 && args[0] instanceof Throwable) {
-            return (Throwable) args[0];
-        }
-        return null;
-    }
+   public static Throwable getThrowable(Object[] args) {
+      if (args.length > 0 && args[0] instanceof Throwable) {
+         return (Throwable) args[0];
+      }
+      return null;
+   }
 
-    public static String getMessage(Object[] args) {
-        return Args.format(args);
-    }
+   public static String getMessage(Object[] args) {
+      return Args.format(args);
+   }
 
-    public static String getMessage(Throwable e) {
-        return String.format("%s: %s", e.getClass().getSimpleName(), e.getMessage());
-    }
-    
-    public static RuntimeException newRuntimeException(Object ... args) {
-        if (args.length == 1) {
-            Throwable e = getThrowable(args);
-            if (e instanceof RuntimeException) {
-                return (RuntimeException) e;
-            }
-        }
-        return new ArgsRuntimeException(args);
-    }
+   public static String getMessage(Throwable e) {
+      if (e.getMessage() == null) {
+         return e.getClass().getSimpleName();
+      } else {
+         return String.format("%s: %s", e.getClass().getSimpleName(), e.getMessage());
+      }
+   }
 
-    public static String printStackTrace(Throwable exception) {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        PrintStream ps = new PrintStream(baos);
-        try {
-            exception.printStackTrace(ps);
-            return baos.toString(Strings.ENCODING);
-        } catch (UnsupportedEncodingException e) {
-            throw Exceptions.newRuntimeException(e);
-        }
-    }
-    
-    public static void warn(Exception e) {
-        e.printStackTrace(System.err);
-    }
-    
+   public static RuntimeException newRuntimeException(Object... args) {
+      if (args.length == 1) {
+         Throwable e = getThrowable(args);
+         if (e instanceof RuntimeException) {
+            return (RuntimeException) e;
+         }
+      }
+      return new ArgsRuntimeException(args);
+   }
+
+   public static String printStackTrace(Throwable exception) {
+      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+      PrintStream ps = new PrintStream(baos);
+      try {
+         exception.printStackTrace(ps);
+         return baos.toString(Strings.ENCODING);
+      } catch (UnsupportedEncodingException e) {
+         throw Exceptions.newRuntimeException(e);
+      }
+   }
+
+   public static void warn(Exception e) {
+      e.printStackTrace(System.err);
+   }
+
 }
